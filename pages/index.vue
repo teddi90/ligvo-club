cea20c79ee3cfa485d18a7e22028eeb04e92227f
teddi90 <slobodyanoleg5@gmail.com>
2023-05-18 18:14:16 +0300

    add hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-    <div></div>
+    <section class="hero">
+        <div class="container">
+            <div class="hero__wrapper">
+                <h1 class="hero__title">
+                    Клуб<br />
+                    настільних ігор
+                </h1>
+                <div class="hero__btn-wrapper">
+                    <a class="hero__btn" href="#">Забронювати гру</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="catalog">
+        <div class="container">
+            <h2 class="title">Каталог ігор</h2>
+        </div>
+    </section>
 </template>
 
 <script setup>
