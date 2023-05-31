e5334dccbceeb651a8ecd1ec355c947bd5b34116
teddi90 <slobodyanoleg5@gmail.com>
2023-05-31 18:30:57 +0300

    json restart command

--- stores/games.js
+++ stores/games.js
@@ -4,8 +4,8 @@ export const useGamesStore = defineStore('games', {
     }),
     actions: {
         async fetchBoardGames() {
-            // const data = await useWpApi().get('boardgames');
-            // this.boardGames = data;
+            // const { data: resp } = await useWpApi().get('boardgames');
+            // this.boardGames = resp.value;
             // console.log(data.value)
             const { data: resp } = await useFetch('https://ligvo-cms.grt-team.com/wp-json/wp/v2/boardgames');
             this.boardGames = toRaw(resp.value);
